6cc97ecf79db5d5f6b57cc5be9948287a816fec1
thifanyleticiaifpr-cyber <thifany.leticia.ifpr@gmail.com>
2025-11-01 16:12:40 -0300

    a
    botoes

--- html/js/bloconotas.js
+++ html/js/bloconotas.js
@@ -67,3 +67,15 @@ Hospedar e deixar acessível. */
                 alert("Não há notas para apagar")
             }
         }
+
+
+/*Um campo de texto e um botão “Adicionar”.
+Um botão para marcar a nota como: não urgente ou urgente.
+Cada nota digitada deve aparecer dentro de um box. As não urgentes em amarelo limão e as  urgentes em vermelho.
+As notas devem ser coladas em formato de lista.
+Cada nota a ser inserida seguindo padrão de cores: Azul, Verde, Roxo.  
+Botões para remover nota a nota.
+Um botão “Apagar tudo” remove todas as notas.
+Estilizar todo o conteúdo.
+Hospedar e deixar acessível.
+ */
